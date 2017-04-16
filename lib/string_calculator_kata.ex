d157b6bc3667e8a321889c32007124a3fec65358
Andrea <andrea.bergamini91@gmail.com>
2017-04-16 19:36:51 +0200

    Added custom delimiters

--- lib/string_calculator_kata.ex
+++ lib/string_calculator_kata.ex
@@ -3,16 +3,21 @@ defmodule StringCalculatorKata do
     0
   end
 
-  def add(string) do
-    delimiters = ["\n", ","]
-    #String.split(string, ["\n", ","]) 
-    #  |> Enum.reduce(0, fn(x, sum) 
-    #    -> String.to_integer(x) + sum 
-    #  end)
-    string |> String.split(delimiters) |> sum
+  def add("//" <> string_without_header) do
+    [custom_delimiter, string] = String.split(string_without_header, "\n")
+    string |> add(custom_delimiter)
   end
 
-  def sum(list) do
+  def add(string) do
+    default_delimiters = ["\n", ","]
+    string |> add(default_delimiters)
+  end
+
+  defp add(string, delimiters) do
+    string |> String.split(delimiters) |> sum_all_values
+  end
+
+  defp sum_all_values(list) do
     Enum.reduce(list, 0, fn(x, sum) ->
       sum + String.to_integer(x)
     end)
--- test/string_calculator_kata_test.exs
+++ test/string_calculator_kata_test.exs
@@ -16,4 +16,8 @@ defmodule StringCalculatorKataTest do
   test "new line as delimiter" do
   	assert StringCalculatorKata.add("1\n2,3") == 6
   end
+
+  test "custom delimiter" do
+  	assert StringCalculatorKata.add("//;\n1;2;3") == 6
+  end
 end
